f751fb81e96b3e6285558c51a22b35325c010b23
ZashRin <itgtktdbx@gmail.com>
2020-09-21 17:35:31 +0300

    feat: add min function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array .length === 0) return 0;
+  array.sort((a, b) => b - a);
+  return array.pop();
 }
 
 exports.max = function max (array) {
